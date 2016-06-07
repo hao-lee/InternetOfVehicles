a9e19dabe3179d1216171b6b0e73cade63da952f
Hao Lee <haolee.swjtu@gmail.com>
2016-06-07 08:43:36 +0800

    第一个可用版本

--- src/algorithm/MapCaching.java
+++ src/algorithm/MapCaching.java
@@ -1,108 +1,228 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class MapCaching implements ProbabilityInterface{
+public class MapCaching {
 
 	ArrayList<ArrayList<Double>> phiList = null;
 	ArrayList<ArrayList<Integer>> resList = new ArrayList<>();
-	
+
+	int k = 15;// 15块数据(gz_05/14)
+	int i = 3;// 3个节点
+
 	public static void main(String[] args) {
-		new MapCaching().map_caching();
+		// new MapCaching().map_caching();
 	}
-	
+
+	/**
+	 * 随机概率构造函数
+	 */
+	public MapCaching(int[] probility_Data_I, int[] lodeData_X, int[] lodeData_Y) {
+		super();
+		ProbabilityInterface proIf = new ProbabilityImp(probility_Data_I,
+				lodeData_X, lodeData_Y);
+		phiList = proIf.getPhiList(k, i);
+	}
+
+	/**
+	 * 文件概率构造函数
+	 */
+	public MapCaching() {
+		super();
+		int[] probility_Data_I = {}; // 存放路过i节点的次数
+		int[] lodeData_X = {}; // 存放数据。每个节点下载chunk的个数Xi
+		int[] lodeData_Y = {}; // 存放数据。每个节点下载的最后一个chunk的编号Yi
+		/* 调用接口函数，生成新的概率 */
+		BufferedReader bf = null;
+		try {
+			FileInputStream data_i = new FileInputStream("./IOFile/DATA_I.txt");
+			bf = new BufferedReader(new InputStreamReader(data_i));
+			String line;
+			System.out.println("读取路过i节点次数！");
+			while ((line = bf.readLine()) != null) {
+				probility_Data_I = String2Int(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("打开路过节点次数文件失败！");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("读取路过节点次数文件失败！");
+			e.printStackTrace();
+		} finally {
+			if (bf != null) {
+				try {
+					bf.close();
+				} catch (IOException e1) {
+					System.out.println("DATA_I字节流关闭失败！");
+				}
+			}
+		}
+
+		// lodeData_X.txt
+		try {
+			if (probility_Data_I.length == 0 || probility_Data_I == null) {
+				System.out.println("经过i点的次数为空！");
+			} else {
+				FileInputStream fio = new FileInputStream(
+						"./IOFile/lodeData_X.txt");
+				bf = new BufferedReader(new InputStreamReader(fio));
+				String line;
+				int len = probility_Data_I.length;
+				int i = 0;
+				System.out.println("读取第i节点下载chunk的个数Xi");
+
+				while ((line = bf.readLine()) != null) {
+					
+					lodeData_X = String2Int(line);
+					if (lodeData_X.length != len) {
+						System.out.println("Waring:长度不符！");
+						break;
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println("打开在X点下载块数文件失败！");
+			e.printStackTrace();
+		} finally {
+			if (bf != null) {
+				try {
+					bf.close();
+				} catch (IOException e1) {
+					System.out.println("Lode_X字节流关闭失败！");
+				}
+			}
+		}
+
+		// lodeData_Y.txt
+		try {
+			if (probility_Data_I.length == 0 || probility_Data_I == null) {
+				System.out.println("经过i点的次数为空！");
+			} else {
+				FileInputStream fio = new FileInputStream(
+						"./IOFile/lodeData_Y.txt");
+				bf = new BufferedReader(new InputStreamReader(fio));
+				String line;
+				int len = probility_Data_I.length;
+				int i = 0;
+				System.out.println("第i个节点下载的最后一个chunk的编号Yi");
+
+				while ((line = bf.readLine()) != null) {
+				
+					lodeData_Y = String2Int(line);
+					if (lodeData_Y.length != len) {
+						System.out
+								.println("Waring:输入的每个节点下载最后一个节点的数据和输入节点数不符！");
+						break;
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("打开在节点下载最后一个块的数据文件失败！");
+			e.printStackTrace();
+		} finally {
+			if (bf != null) {
+				try {
+					bf.close();
+				} catch (IOException e1) {
+					System.out.println("Lode_Y字节流关闭失败！");
+				}
+			}
+		}
+
+		ProbabilityInterface proIf = new ProbabilityImp(probility_Data_I,
+				lodeData_X, lodeData_Y);
+		phiList = proIf.getPhiList(k, i);
+	}
+
+	private int[] String2Int(String line) {
+		String tmp[] = line.trim().split(",");
+		int len = tmp.length;
+		int reslut[] = new int[len];
+		for (int i = 0; i < tmp.length; i++) {
+			try {
+				reslut[i] = Integer.parseInt(tmp[i].trim());
+			} catch (Exception e) {
+				System.out.println("格式转换错误！");
+			}
+			System.out.print(reslut[i] + " ");
+		}
+		System.out.println();
+		return reslut;
+	}
+
 	/**
 	 * 算法计算主函数
 	 */
 	public void map_caching() {
 		double threshold = 0.5;
-		int k = 5;//5块数据
-		int i = 3;//3个节点
-		phiList = getPhiList(k, i);//获取概率二维列表
-		//遍历每个数据块
-		for(int count = 0; count < k; count++){
-			
+
+		// 遍历每个数据块
+		for (int count = 0; count < k; count++) {
+
 			double pk = 0;
 			ArrayList<Double> fList = new ArrayList<>();
-			//将PhiList的第count行复制到fList中，fList表示第count块数据在各个节点的概率集合
-			for(int tmp = 0; tmp < phiList.get(count).size(); tmp++){
+			// 将PhiList的第count行复制到fList中，fList表示第count块数据在各个节点的概率集合
+			for (int tmp = 0; tmp < phiList.get(count).size(); tmp++) {
 				fList.add(phiList.get(count).get(tmp));
 			}
 			ArrayList<Integer> sList = new ArrayList<>();
-			while(!fListIsEmpty(fList)&& pk <threshold){
+			while (!fListIsEmpty(fList) && pk < threshold) {
 				ArrayList<Double> ret = getMaxValue(fList);
 				double pTop = ret.get(0);
-				int iTop = (int)(ret.get(1).doubleValue());
-				fList.set(iTop, (double)-1);
+				int iTop = (int) (ret.get(1).doubleValue());
+				fList.set(iTop, (double) -1);
 				pk = pk + pTop;
 				sList.add(iTop);
 			}
-			resList.add(sList);//每测试一个块k，都会在resList中加入一行
-			
-		}//for
-		//循环结束后，resList就是结果的二维列表
-		for(int local = 0; local < resList.size(); local++)
-			System.out.println("第"+local+"块所要缓存到的节点号："+resList.get(local).toString());
+			resList.add(sList);// 每测试一个块k，都会在resList中加入一行
+
+		}// for
+			// 循环结束后，resList就是结果的二维列表
+		for (int local = 0; local < resList.size(); local++)
+			System.out.println(resList.get(local).toString());
 		return;
 	}
-	
+
 	/**
 	 * 每当从fList取出一个当前的最大概率，为了不破坏顺序，不直接将该概率删除，而是赋值为-1
 	 * 当所有元素都为-1时就可以认为fList所有概率都被取出，列表空了，返回true
-	 * @param fList		某个块在各个节点的概率列表fList
-	 * @return			fList是否为空
+	 * 
+	 * @param fList
+	 *            某个块在各个节点的概率列表fList
+	 * @return fList是否为空
 	 */
-	private boolean fListIsEmpty(ArrayList<Double> fList){
-		for(int local =0; local < fList.size(); local++){
-			if(fList.get(local) != -1)
-				return false;//没空
+	private boolean fListIsEmpty(ArrayList<Double> fList) {
+		for (int local = 0; local < fList.size(); local++) {
+			if (fList.get(local) != -1)
+				return false;// 没空
 		}
 		return true;
 	}
-	
 
 	/**
-	 * @param fList		某个块在各个节点的概率列表fList
-	 * @return			两个元素的list，第一个元素是pTop，第二个元素是iTop
+	 * @param fList
+	 *            某个块在各个节点的概率列表fList
+	 * @return 两个元素的list，第一个元素是pTop，第二个元素是iTop
 	 */
-	private ArrayList<Double> getMaxValue(ArrayList<Double> fList){
+	private ArrayList<Double> getMaxValue(ArrayList<Double> fList) {
 		double pTop = -1;
 		int iTop = -1;
 		ArrayList<Double> ret = new ArrayList<Double>();
-		for(int local=0; local < fList.size(); local++){
-			if(fList.get(local) > pTop)
-				{pTop = fList.get(local);iTop = local;}
+		for (int local = 0; local < fList.size(); local++) {
+			if (fList.get(local) > pTop) {
+				pTop = fList.get(local);
+				iTop = local;
+			}
 		}
 		ret.add(pTop);
-		ret.add((double)iTop);//最大概率以及它对应的节点号
+		ret.add((double) iTop);// 最大概率以及它对应的节点号
 		return ret;
 	}
-	
-	/**
-	 * 该接口内的方法必须被实现，这是获得初始概率二维列表的方法
-	 */
-	@Override
-	public ArrayList<ArrayList<Double>> getPhiList(int k,int i) {
-		
-		ArrayList<ArrayList<Double>> PhiList = new ArrayList<>();
-		ArrayList<Double> tmp = null;
-		//第i个块 缓存在第j个节点的概率
-		double[][] phiTmp = {
-				{0.1,0.3,0.6},
-				{0.2,0.1,0.2},
-				{0.1,0.5,0.3},
-				{0.7,0.1,0.1},
-				{0.3,0.5,0.2},
-		};
-		for(int k_tmp = 0; k_tmp < k; k_tmp++){
-			//第k块在各个i节点的概率一维列表
-			tmp = new ArrayList<>();
-			for(int i_tmp = 0;i_tmp < i; i_tmp++){
-				tmp.add(phiTmp[k_tmp][i_tmp]);
-			}
-			PhiList.add(tmp);//第k个块的数据填充完毕，加入集合中
-		}
-		return PhiList;
-	}//getPhiArray
 
 }
---
+++ src/algorithm/ProbabilityImp.java
@@ -0,0 +1,157 @@
+package algorithm;
+
+import java.util.ArrayList;
+
+public class ProbabilityImp implements ProbabilityInterface {
+
+	public static final int MAXSIZE_X = 8;//节点缓存的chunk数量
+	public static final int CHUNK_NUMBER = 15;//文本被分的chunk数量
+	public static final int NODE_NUMBER = 3;//节点个数
+	
+	public double[][] probility_Data = new double[CHUNK_NUMBER][NODE_NUMBER];
+	//phi(i,k)
+	public double[][] DownloadTimes_X = new double[NODE_NUMBER][MAXSIZE_X + 1];
+	//路过节点时，下载的chunk数量为Xi的次数
+    public double[][] DownloadTimes_Y = new double[NODE_NUMBER][CHUNK_NUMBER + 1];
+    //路过节点时，下载的最后一个chunk的编号为Yi的次数
+	public double[] PassTimes={0,0,0};// 存放路过i节点的次数
+	//public int[] UpdatePassTimes;
+	public int[] updataNodeSequence;//行驶中，一个文本下载完成经过的节点顺序（外部传参）
+	
+	int[] lodeData_X;  //每个节点下载chunk的个数Xi（外部传参）
+	int[] lodeData_Y; //每个节点下载的最后一个chunk的编号Yi（外部传参）
+	
+	/**
+	 * 用于传入初始化数据
+	 * @param int[][] lodeData_X 第i节点下载chunk的个数Xi
+	 * @param probility_Data_Y 第i节点下载的最后一个chunk的编号Yi
+	 * @param probility_Data_I 存放路过i节点的次数
+	 */
+	public ProbabilityImp(int[] updataNodeSequence, int[] lodeData_X,
+			int[] lodeData_Y) {
+		super();
+		this.updataNodeSequence = updataNodeSequence;//这里传递的是节点的编号（1,2,3...）。
+		this.lodeData_X = lodeData_X;
+		this.lodeData_Y = lodeData_Y;
+		//以上三者的长度应当是一样的
+	}
+
+	@Override
+	public ArrayList<ArrayList<Double>> getPhiList(int k, int i) {
+	
+		for (int local = 0; local<updataNodeSequence.length; local++) { //更新DownloadTimes_X
+			
+				if(lodeData_X[local]>=MAXSIZE_X){
+					DownloadTimes_X[updataNodeSequence[local]-1][MAXSIZE_X]++;
+					//减1是因为节点从1开始，下标从0开始
+				}else{
+					DownloadTimes_X[updataNodeSequence[local]-1][lodeData_X[local]]+=1;	
+					//减1是因为节点从1开始，下标从0开始
+				}				
+		} // for(int n = 0;	
+		
+		for (int local = 0; local<updataNodeSequence.length; local++) {//更新DownloadTimes_Y		
+				DownloadTimes_Y[updataNodeSequence[local]-1][lodeData_Y[local]]+=1;	
+				//减1是因为节点从1开始，下标从0开始
+		} // for(int n = 0;
+			
+		for (int local = 0; local<updataNodeSequence.length; local++) {//更新路过次数
+			 PassTimes[updataNodeSequence[local]-1]+=1;
+			//减1是因为节点从1开始，下标从0开始
+		} 
+		for (int kChunk = 0; kChunk < CHUNK_NUMBER; kChunk++) {//利用公式计算Phi（i，k）
+			for (int local = 0; local<updataNodeSequence.length; local++) {
+				probility_Data[kChunk][updataNodeSequence[local]-1] = 
+						     p(kChunk+1, updataNodeSequence[local],local);//只改变最新路过的节点的概率
+				              //传送的是chunk编号（注意不是下标，下标+1=编号）
+			}
+		}		
+
+		//////////////////////////////////	
+		System.out.println("最终的DownloadTimes_X如下：");
+		for ( i = 0; i < NODE_NUMBER; i++) // 打印概率
+		{
+			for (int j = 0; j <= MAXSIZE_X; j++) {
+				System.out.print(DownloadTimes_X[i][j] + "    ");
+			}
+			System.out.print("\n");
+		}
+		//////////////////////////////////
+		System.out.println("最终的DownloadTimes_Y如下：");
+		for ( i = 0; i < NODE_NUMBER; i++) // 打印概率
+		{
+			for (int j = 0; j <= CHUNK_NUMBER; j++) {
+				System.out.print(DownloadTimes_Y[i][j] + "    ");
+			}
+			System.out.print("\n");
+		}
+		/////////////////////////////
+		System.out.println("最终的PassTimes如下：");
+		for ( i = 0; i < NODE_NUMBER; i++) // 打印概率
+		{
+			System.out.print(PassTimes[i]+"    "); 
+		}	
+		///////////////////////////
+		System.out.println("\n最终的概率如下：");
+		for ( i = 0; i < CHUNK_NUMBER; i++) // 打印概率
+		{
+			for (int j = 0; j < NODE_NUMBER; j++) {
+				String double_str = String.format("%.4f", probility_Data[i][j]);
+				System.out.print(double_str + "    ");
+			}
+			System.out.print("\n");
+		}
+		///////////////////////////////
+		// 对数组形式的=》转成ArrayList
+		ArrayList<ArrayList<Double>> prob = new ArrayList<ArrayList<Double>>();
+
+		for (int j = 0; j < probility_Data.length; j++) {
+			ArrayList<Double> pro = new ArrayList<Double>();
+			for (int j2 = 0; j2 < probility_Data[0].length; j2++) {
+				pro.add(probility_Data[j][j2]);
+			}
+			prob.add(pro);
+		}
+		return prob;
+	}
+	
+/*
+ * 函数功能：计算节点下载chunk的概率
+ * 入口参数：
+ * （1）iNode：节点编号（注意不是下标，下标从0开始）
+ * （2）kChunk：chunk编号（注意不是下标，下标从0开始）
+ * 返回值：double类型，iNode节点下载kChunk的概率值
+ */
+	public double p(int kChunk, int iNode,int local) {
+		double phi = 0;
+		double pdx = 0;
+		int priorNode ;
+		if(PassTimes[iNode-1]<=0.1)//防止分母PassTimes[m]为0
+		{
+			return 0;
+		}else if ((iNode == updataNodeSequence[0])|(local==0)) {//当节点为第一个路过的节点时
+			
+			int v = 0;
+			for (v = MAXSIZE_X; v >= kChunk; v--) {
+					           // 下载chunk个数为Xi的次数/路过的总次数
+					phi+= DownloadTimes_X[iNode-1][v]/PassTimes[iNode-1];//公式的特殊情况处理		
+				}
+
+		} else {
+				int j = 0;
+				int v = 0;
+				priorNode = updataNodeSequence[local-1];
+				for (j = 0; j < kChunk; j++) {
+					for (v = MAXSIZE_X; v >= kChunk - j; v--) {
+						           // 下载chunk个数为Xi的次数/路过的总次数
+						pdx += DownloadTimes_X[iNode-1][v]/PassTimes[iNode-1];		
+					}
+					                // 下载chunk最后一个编号为Yi的次数/路过的总次数
+					phi += pdx*(DownloadTimes_Y[priorNode-1][j]/PassTimes[priorNode-1]);
+					//updataNodeSequence[local-1]是iNode的上一节点，local是iNode节点的下标
+				}
+			}
+		return phi;
+	}//p
+	
+}
